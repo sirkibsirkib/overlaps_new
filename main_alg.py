390ea23d2f88357f119f0346288ffbe8d957363e
sirkibsirkib <christopher.esterhuyse@gmail.com>
2017-04-26 15:54:51 +0200

    work continues on the output format

--- main_alg.py
+++ main_alg.py
@@ -57,7 +57,7 @@ def align_using_cigar(a, b, cigar):
 def print_solution(sol):
 	pass
 	# # TODO candidates have been changed!
-	# a_index, b_index, a_ovr, b_ovr, b_tail, cigar, debug_str = sol
+	# a_index, b_index, a_ovr, b_ovr, , cigar, debug_str = sol
 	# print('sol', sol)
 	# a = S_dict[sol[0]]
 	# if sol[6]: a = a[::-1]
@@ -66,11 +66,11 @@ def print_solution(sol):
 	# print('a (pref) = ' +a)
 	# print('b (suff) = ' +b)
 	# a_head = a[:len(a)-sol[2]]
-	# b_tail = b[sol[3]:]
+	#  = b[sol[3]:]
 	# cigar = sol[5]
 	# a_aligned, b_aligned = align_using_cigar(a[-sol[2]:], b[:sol[3]], cigar)
 	# print(a_head + '(' + a_aligned + ')')
-	# print(((len(a_head)) * ' ') + '(' + b_aligned + ')' + b_tail)
+	# print(((len(a_head)) * ' ') + '(' + b_aligned + ')' + )
 	# print()
 
 def string_id_to_T_index_map(S):
@@ -174,10 +174,9 @@ def verifies(candidate, T, b_len, arguments):
 	#TODO make sure the b_len arg is correctly giving the length of string B (suffix / found) string
 	try:
 		a_index, b_index, a_ovr, b_ovr, b_tail, debug_str = candidate
-
 		errs_allowed = math.floor(max(a_ovr, b_ovr)*arguments.e)
 		b_ovr_start = b_index - b_ovr - b_tail + b_len
-		print(candidate)
+		# print(candidate)
 		a_ovr_string = T[a_index : a_index + a_ovr]
 		b_ovr_string = T[b_ovr_start : b_index + b_ovr - b_tail]
 
@@ -190,13 +189,13 @@ def verifies(candidate, T, b_len, arguments):
 			for i in range(a_ovr):
 				if T[a_index+i] != T[b_ovr_start+i]:
 					err_count += 1
-			return err_count <= errs_allowed, str(a_ovr) + 'M'
+			return err_count if err_count <= errs_allowed else -1, str(a_ovr) + 'M'
 	except:
 		#out of string bounds exception
 		print('OH CRAP, out of bounds verify!! oh noes!')
 		raise
 
-def get_solutions(candidate_set, T, index_to_length_map, index_to_id_map, id_to_names_map, arguments):
+def get_solutions(candidate_set, S_dict, T, index_to_length_map, index_to_id_map, id_to_names_map, arguments):
 	solution_set = set()
 	for candidate in candidate_set:
 		b_len = index_to_length_map[candidate[1]]
@@ -204,25 +203,39 @@ def get_solutions(candidate_set, T, index_to_length_map, index_to_id_map, id_to_
 
 		if k != -1: #-1 errors means 'too many to verify'
 			#solution accepted
+			print('\n\n\n>')
+			a_len = index_to_length_map[candidate[0]]
 			a_index, b_index, a_ovr, b_ovr, b_tail, debug_str = candidate
+			print('pref', T[a_index:a_index+a_len])
+			print('suff', T[b_index:b_index+b_len])
 			a_id = index_to_id_map[a_index]
 			b_id = index_to_id_map[b_index]
+
+
+			OLA = a_ovr
+			OLB = b_ovr
+			OHA = -(a_len - a_ovr)
+			OHB = -(b_len - b_ovr) if b_tail == 0 else b_tail
+			if a_id % 2 == 1:
+				OHA *= -1
+				OHB *= -1
+				a_id, b_id = b_id, a_id
+
+			CIGAR = cigar
+			K = k
+			O = 'N' if b_id%2==0 else 'I'
 			a_name = id_to_names_map[a_id]
 			b_name = id_to_names_map[b_id]
-			if not arguments.inverts:
-				a_flip = b_flip = False
-			else:
-				a_flip = a_id % 2 == 1
-				b_flip = b_id % 2 == 1
+			solution = (a_name, b_name, O, OHA, OHB, OLA, OLB, K, CIGAR)
+			print('a\tb\tO\tOHA\tOHB\tOLA\tOLB\tK\tCIG')
+			print(*solution, sep='\t')
+			print(candidate)
+			print(' ==> ', solution)
+			print(' '*(-OHA), S_dict[a_name])
+			second = S_dict[b_name] if O=='N' else S_dict[b_name][::-1]
+			print(' '*OHA, second)
 
-			if a_id > b_id:
-				# need to swap around the solutions
-				pass
-			#idA idB O OHA OHB OLA OLB K
-			c = c[:5] + (cigar,) + c[6:]
-			print('AFTER VERIFICATION', c)
-			solution_set.add(c)
-	return solution_set
+	return sorted(solution_set)
 
 '''
 Given S_dict, a dict with values being strings.
@@ -280,7 +293,7 @@ def overlaps(S_dict, arguments):
 	t2 = time.clock()
 	print('step1 time', t2-t1)
 	#STEP 2
-	solution_set = get_solutions(candidate_set, T, index_to_length_map, index_to_id_map, id_to_names_map, arguments)
+	solution_set = get_solutions(candidate_set, S_dict, T, index_to_length_map, index_to_id_map, id_to_names_map, arguments)
 
 	t3 = time.clock()
 	print('step2 time', t3-t2)
@@ -291,39 +304,7 @@ def overlaps(S_dict, arguments):
 	print('step 1 precision', len(solution_set) / (len(candidate_set)) if len(candidate_set) > 0 else 1)
 	print('verified set', solution_set)
 
-
-	solutions = []
-	for sol in solution_set:
-
-		# TODO candidates have been changed!
-		a_id = index_to_id_map[sol[0]]
-		b_id = index_to_id_map[sol[1]]
-		if not arguments.inverts:
-			a_flip = b_flip = False
-		else:
-			a_flip = a_id % 2 == 1
-			b_flip = b_id % 2 == 1
-
-		print('WILL BE SOLUTION', sol)
-
-		a_name = id_to_names_map[a_id]
-		b_name = id_to_names_map[b_id]
-		a = S_dict[a_name]
-		b = S_dict[b_name]
-		if a_flip: a = a[::-1]
-		if b_flip: b = b[::-1]
-		print(' '*(-sol[3]+len(b)) + a)
-		print(b)
-		# sol = (a_name, b_name, v[2], v[3], v[4], v[5], a_flip, b_flip, v[6])
-		# solutions.append(sol)
-
-
-	# solutions = [(id_to_names_map[index_to_id_map[v[0]]],
-	# 		 id_to_names_map[index_to_id_map[v[1]]],
-	# 		 v[2], v[3], v[4], v[5], 'same' if not arguments.inverts or (v[0]+v[1])%2==0 else 'opp')
-	# 		for v in verified_set]
-
-	return sorted(solutions)
+	return sorted(solution_set)
 
 
 '''SCRIPT BEGIN'''
